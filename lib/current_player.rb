5b603110c7574500e9fae1a06b0e664d402c3654
Mika88 <mikaspivak@gmail.com>
2018-07-12 02:54:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,8 @@ def turn_count(board)
       board = counter
 end
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  counter = turn_count(board)
+  if counter % 2 == 0
     puts "X"
   else
     puts "O"
